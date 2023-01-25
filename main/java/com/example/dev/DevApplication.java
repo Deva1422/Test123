0aa0ae51fbbeb5d51d0722a998d68d0a07402995
Deva1422 <pvdevakumar142@gmail.com>
2023-01-25 22:46:59 +0530

    Test123

---
+++ main/java/com/example/dev/DevApplication.java
@@ -0,0 +1,23 @@
+package com.example.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DevApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "nameee", defaultValue = "World!") String nam) {
+		return String.format("Hello %s!", nam);
+	}
+
+}
